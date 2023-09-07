a122a2c41b3ddce0fb8c1cbef019254dc1e22685
Kevin Quispe <kevincarlosqa@gmail.com>
2023-09-07 09:26:01 -0500

    add: filteredLogs

--- index.js
+++ index.js
@@ -22,11 +22,36 @@ const exerciseSchema = new Schema({
   username: String,
   description: String,
   duration: Number,
-  date: String,
+  date: Date,
 });
 
 const User = mongoose.model("User", userSchema);
 const Exercise = mongoose.model("Exercise", exerciseSchema);
+
+function convertirFecha(fechaISO) {
+  const meses = [
+    "Jan",
+    "Feb",
+    "Mar",
+    "Apr",
+    "May",
+    "Jun",
+    "Jul",
+    "Aug",
+    "Sep",
+    "Oct",
+    "Nov",
+    "Dec",
+  ];
+
+  const date = new Date(fechaISO);
+  const diaSemana = date.toLocaleDateString("en-US", { weekday: "short" });
+  const mes = meses[date.getUTCMonth()];
+  const dia = date.getUTCDate();
+  const anio = date.getUTCFullYear();
+
+  return `${diaSemana} ${mes} ${dia} ${anio}`;
+}
 
 const users = [];
 app.post("/api/users", async function (req, res) {
@@ -45,12 +70,12 @@ app.post("/api/users/:_id/exercises", async function (req, res) {
   try {
     // console.log(req.params);
     const findUser = await User.findById(req.params._id);
-
+    const parsedDay = convertirFecha(req.body.date);
     let newExercise = new Exercise({
       username: findUser.username,
       description: req.body.description,
       duration: +req.body.duration,
-      date: req.body.date || Date.now(),
+      date: new Date(req.body.date) || Date.now(),
     });
     const savedExercise = await newExercise.save();
     console.log(savedExercise);
@@ -58,7 +83,7 @@ app.post("/api/users/:_id/exercises", async function (req, res) {
     res.json({
       _id: req.params._id,
       username: savedExercise.username,
-      date: savedExercise.date,
+      date: parsedDay,
       duration: savedExercise.duration,
       description: savedExercise.description,
     });
@@ -71,6 +96,92 @@ app.get("/api/users", async function (req, res) {
   try {
     let findUser = await User.find({});
     res.json(findUser);
+  } catch (error) {
+    res.json({ error });
+  }
+});
+
+function filtrarPorFecha(array, fechaDesde, fechaHasta) {
+  // Convertir las fechas desde y hasta al formato "Thu Feb 02 2023"
+  if (fechaDesde) {
+    fechaDesde = convertirFecha(fechaDesde);
+  }
+
+  if (fechaHasta) {
+    fechaHasta = convertirFecha(fechaHasta);
+  }
+
+  // Filtrar el array original según las fechas especificadas
+  const resultado = array.filter((objeto) => {
+    const fechaObjeto = objeto.date;
+    if (fechaDesde && fechaHasta) {
+      return fechaObjeto >= fechaDesde && fechaObjeto <= fechaHasta;
+    } else if (fechaDesde) {
+      return fechaObjeto >= fechaDesde;
+    } else if (fechaHasta) {
+      return fechaObjeto <= fechaHasta;
+    } else {
+      return true; // Si no se proporciona ninguna fecha, no se aplica filtro
+    }
+  });
+
+  return resultado;
+}
+
+app.get("/api/users/:_id/logs?", async function (req, res) {
+  try {
+    const { from, to, limit } = req.query;
+    console.log(from, to, limit);
+    let findUser = await User.findById(req.params._id);
+    let logs = await Exercise.find({ username: findUser.username })
+      .select("-_id description duration date")
+      .limit(limit);
+    // logs[date] = convertirFecha(logs.date);
+    const convertedLogs = logs.map((objeto) => ({
+      description: objeto.description,
+      duration: objeto.duration,
+      date: convertirFecha(objeto.date),
+    }));
+    if (from && to) {
+      let filteredLogs = filtrarPorFecha(convertedLogs, from, to);
+      res.json({
+        _id: findUser._id,
+        from,
+        to,
+        username: findUser.username,
+        count: filteredLogs.length,
+        log: filteredLogs,
+      });
+      return;
+    }
+    if (from) {
+      let filteredLogs = filtrarPorFecha(convertedLogs, from, null);
+      res.json({
+        _id: findUser._id,
+        from,
+        username: findUser.username,
+        count: filteredLogs.length,
+        log: filteredLogs,
+      });
+      return;
+    }
+    if (to) {
+      let filteredLogs = filtrarPorFecha(convertedLogs, from, null);
+      res.json({
+        _id: findUser._id,
+        to,
+        username: findUser.username,
+        count: filteredLogs.length,
+        log: filteredLogs,
+      });
+      return;
+    }
+    res.json({
+      _id: findUser._id,
+      username: findUser.username,
+      count: logs.length,
+      log: convertedLogs,
+    });
   } catch (error) {
     res.json({ error });
   }
